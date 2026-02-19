a5a1cbd722d4fb0cf896bd6e52d383f9b660f632
rmdna <04rodmedina@gmail.com>
2026-02-19 06:56:08 -0600

    Added a model for the photos, retiring for tn

--- backend/photos/models.py
+++ backend/photos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to="photos/") # Django will place uploaded files under a photos/ subfolder within my media storage
+    
