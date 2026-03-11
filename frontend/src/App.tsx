dbe9c1d7c7ebaead16cb94f9a5a57d5f259a75b4
rmdna <04rodmedina@gmail.com>
2026-03-11 05:46:52 -0500

    Added a useEffect to App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,18 @@ function App() {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    fetch('http://localhost:8000/api/photos/')
+    .then(res => {if (!res.ok) throw new Error('Failed to fetch photos'); // if backend returns an error, stop and throw it
+      return res.json(); // convert the raw response into usable JavaScript data
+    })
+    .then(data => {
+      setPhotos(data); // store the fetched photos into our photos array
+      setLoading(false); // data has arrived, turn off the loading state
+    })
+
+  }, []);
+
   return (
     <>
       <div>
